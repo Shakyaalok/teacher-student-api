dc3ceb29e1589144d0edb6cf82f573aa9053bddd
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2024-05-19 02:28:25 +0530

    new student added and updated marks added

--- Controllers/Student.js
+++ Controllers/Student.js
@@ -1,20 +1,37 @@
 const Student = require('../Models/Student');
+const { Op } = require('sequelize');
+
 
 const addStudent = async(req, res) => {
     const { name, marks, subject } = req.body;
 
-    if (!name || !marks || !subject) {
-        return res.status(500).json({ message: 'all fields cannot empty' })
-    }
+    try {
+        if (!name || !marks || !subject) {
+            return res.status(500).json({ message: 'fields cannot empty' })
+        }
 
-    const data = {
-        name: name,
-        marks: marks,
-        subject: subject
-    }
+        const data = {
+            name: name,
+            marks: marks,
+            subject: subject
+        }
+        const isExist = await Student.findOne({
+            where: {
+                [Op.and]: [{ name, subject }]
+            }
+        })
+        if (isExist) {
+            await Student.update({ marks }, { where: { id: isExist.id } });
+            const updatedStudentMarks = await Student.findOne({ where: { id: isExist.id } });
+            return res.status(201).json({ data: updatedStudentMarks, message: 'marks updated succefully!' })
+        }
 
-    const student = await Student.create(data);
-    res.status(201).json({ data: student, message: 'Added Successfully!' })
+        const student = await Student.create(data);
+        res.status(201).json({ data: student, message: 'Added Successfully!' })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'something went wrong' })
+    }
 }
 
 module.exports = { addStudent }
--- Middleware/auth.js
+++ Middleware/auth.js
@@ -9,7 +9,7 @@ const auth = async(req, res, next) => {
     const decodecToken = jwt.verify(token, process.env.SECRET_KEY);
 
     if (decodecToken.teacherId) {
-        await Teacher.findByPk({ where: { id: decodecToken.teacherId } }).then(
+        await Teacher.findByPk(decodecToken.teacherId).then(
             (teacher) => {
                 req.teacher = teacher;
                 next();
